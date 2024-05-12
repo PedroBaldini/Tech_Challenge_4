1a18929bd568e6cfdbae217e46be9e0636646352
Pedro Baldini <pedro.baldini25@gmail.com>
2024-05-12 10:31:54 -0300

    Add files via upload

---
+++ App_Streamlit.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import joblib
+from prophet import Prophet
+from prophet.plot import plot_plotly
+from datetime import datetime, timedelta
+import plotly.graph_objs as go
+
+model = joblib.load('prophet_model_final.pkl')
+
+st.title("Previsao do Preço do Petroleo Brent")
+
+days = st.number_input("Quantos dias para prever?", min_value=1, max_value=365, value=7)
+
+future_dates = model.make_future_dataframe(periods=days)
+
+# Gerar previsão
+forecast = model.predict(future_dates)
+
+# Exibir resultado
+st.write("Previsão do preço em US$ para os próximos {} dias:".format(days))
+st.write(forecast[['ds', 'yhat']].tail(days))
+
+# Plotar previsão
+plot = plot_plotly(model, forecast)
+st.plotly_chart(plot)
